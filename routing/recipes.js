cdb64cf02450fec617132ff657a7be2a25768ff2
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-06-10 17:38:32 +0200

    fixed editing reciepe

--- routing/recipes.js
+++ routing/recipes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const RecipeController = require("../controllers/RecipeController");
 
 const router = express.Router();
+
 router.get("/", RecipeController.getAllRecipes);
 router.get("/add", RecipeController.getAddRecipeForm);
 router.post("/add", RecipeController.addRecipe);
 router.get("/search", RecipeController.searchRecipes);
 router.get("/:id/edit", RecipeController.getEditRecipeForm);
-router.post("/:id/edit", RecipeController.updateRecipe);
+router.post("/:id", RecipeController.updateRecipe);
 router.post("/:id/rating", RecipeController.addRating);
 router.delete("/:id", RecipeController.deleteRecipe);
 router.get("/:id", RecipeController.getRecipeById);
--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/recipes", recipeRoutes);
 app.use("/", homeRoutes);
