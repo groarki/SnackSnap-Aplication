3caa07f03b1f4be79854c4bb08ad599de17fa5e1
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-06-10 16:28:47 +0200

    deleted connection string

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 
-const MONGODB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@snacksnap.o8g13lg.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=SnackSnap`;
+const MONGODB_URI = ``;
 
 if (!DB_USER || !DB_PASS) {
   console.error("Error with authentification data");
